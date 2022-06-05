bdc4430f2d016e9ce97b62defbc7e93ca36c6d5f
HaeJin Lee <seareale@gmail.com>
2022-06-05 18:34:57 +0900

    Update bbox_cutmix.py

--- bbox_cutmix.py
+++ bbox_cutmix.py
@@ -2,280 +2,169 @@
 # Written by seareale
 # https://github.com/seareale
 
-import os
-
 import albumentations as A
 import cv2
 import numpy as np
 import torch
 
-# for logging
-SAVE_DIR = "./bbox_cutmix"
-os.makedirs(SAVE_DIR, exist_ok=True)
-
+IMG_SIZE = 1280
 CONFIG = {
-    "p": 0.7,  # probability of applying bbox cutmix
-    "margin_max": 20,
-    "margin_min": 40,
+    "p": 1,  # probability of applying bbox cutmix
+    "margin_min": IMG_SIZE / 40,
+    "margin_max": IMG_SIZE / 20,
     "aug_vis": 0.5,  # visiblity after augmentation
-    "resize_thres": 10,
+    "resize_size": IMG_SIZE / 10,
 }
 
 
 def load_image(self, i):
-    path = self.img_files[i]
-    im = cv2.imread(path)
-    assert im is not None, "Image Not Found " + path
-    h0, w0 = im.shape[:2]  # org hw
-    r = self.img_size / max(h0, w0)  # ratio
-    if r != 1:  # if sizes are not equal
-        im = cv2.resize(
-            im,
-            (int(w0 * r), int(h0 * r)),
-            interpolation=cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR,
-        )
-    return im, (h0, w0), im.shape[:2]  # im, hw_org, hw_resized
-
-
-def save_image(obj, lb, name, p=None):
-    h = obj.shape[0]
-    w = obj.shape[1]
-
-    img = obj.copy()
-    if p is not None:
-        for bbox in p:
-            img = cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
-        if lb is not None:
-            for bbox in lb:
-                bbox = [
-                    0,
-                    (bbox[1] - bbox[3] / 2) * w,
-                    (bbox[2] - bbox[4] / 2) * h,
-                    bbox[3] * w,
-                    bbox[4] * h,
-                ]
-                bbox = [int(x) for x in bbox]
-                img = cv2.rectangle(
-                    img, (bbox[1], bbox[2]), (bbox[1] + bbox[3], bbox[2] + bbox[4]), (0, 0, 255), 2
-                )
-    else:
-        if lb is not None:
-            lb = [int(x) for x in lb]
-            img = cv2.rectangle(
-                img, (lb[1], lb[2]), (lb[1] + lb[3], lb[2] + lb[4]), (0, 0, 255), 2
+    # loads 1 image from dataset index 'i', returns im, original hw, resized hw
+    im = self.ims[i]
+    if im is None:  # not cached in ram
+        # remove npy load
+        path = self.im_files[i]
+        im = cv2.imread(path)  # BGR
+        assert im is not None, "Image Not Found " + path
+        h0, w0 = im.shape[:2]  # orig hw
+        r = self.img_size / max(h0, w0)  # ratio
+        if r != 1:  # if sizes are not equal
+            im = cv2.resize(
+                im,
+                (int(w0 * r), int(h0 * r)),
+                interpolation=cv2.INTER_AREA if r < 1 and not self.augment else cv2.INTER_LINEAR,
             )
-    cv2.imwrite(f"{name}.jpg", img)
+        return im, (h0, w0), im.shape[:2]  # im, hw_original, hw_resized
+    else:
+        return self.ims[i].copy(), self.im_hw0[i], self.im_hw[i]  # im, hw_original, hw_resized
 
 
-def bbox_cutmix(self, img, labels, h, w, index, cfg=CONFIG):
-    # lables = [class, x,y,w,h]
-    # h, w = height, width of img
+def augment(img, labels):
+    album_aug = A.Compose(
+        [
+            A.HorizontalFlip(p=0.7),
+            A.VerticalFlip(p=0.7),
+            A.ColorJitter(p=0.7),
+            A.ShiftScaleRotate(shift_limit=0.01, scale_limit=0.1, rotate_limit=5, p=0.7),
+            A.RandomRotate90(p=0.7),
+        ],
+        bbox_params=A.BboxParams(
+            format="coco", label_fields=["class_labels"], min_visibility=CONFIG["aug_vis"]
+        ),
+    )
+    transformed = album_aug(image=img, bboxes=[labels[1:]], class_labels=[labels[0]])
+
+    cropped_img = transformed["image"]
+    height = cropped_img.shape[0]
+    width = cropped_img.shape[1]
+
+    if len(transformed["class_labels"]) == 0:
+        return cropped_img, None, height, width
+
+    # bboxes = [(x,y,w,h), (x,y,w,h), ... ]
+    # class_labels = [ c, c, ... ]
+    cropped_lb = [transformed["class_labels"][0]] + list(transformed["bboxes"][0])
+
+    return cropped_img, cropped_lb, height, width
+
+
+def get_random_object(self):
+    load_idx = np.random.randint(len(self.indices))  # idx of training image for bboxcutmix
+    load_img, _, (h_l, w_l) = load_image(self, load_idx)  # img, _, resized_h, resized_w
+    lb = self.labels[load_idx].copy()[np.random.randint(len(self.labels[load_idx]))]
+
+    # random offset margin : left, right, up, down
+    offset_margin = np.random.randint(CONFIG["margin_min"], CONFIG["margin_max"], size=(4))
+
+    # bbox label added offset margin
+    cropped_lb = [
+        max(int((lb[1] - lb[3] / 2) * w_l) - offset_margin[0], 0),
+        min(int((lb[1] + lb[3] / 2) * w_l) + offset_margin[1], w_l),
+        max(int((lb[2] - lb[4] / 2) * h_l) - offset_margin[2], 0),
+        min(int((lb[2] + lb[4] / 2) * h_l) + offset_margin[3], h_l),
+    ]
+
+    # selected object
+    cropped_img = load_img[cropped_lb[2] : cropped_lb[3], cropped_lb[0] : cropped_lb[1]].copy()
+
+    # object label after cropped
+    # class, x, y, w, h
+    cropped_lb = [
+        lb[0],
+        int((lb[1] - lb[3] / 2) * w_l) - cropped_lb[0],
+        int((lb[2] - lb[4] / 2) * h_l) - cropped_lb[2],
+        int(lb[3] * w_l),
+        int(lb[4] * h_l),
+    ]
+
+    cropped_img, cropped_lb, height, width = augment(cropped_img, cropped_lb)
+
+    return cropped_img, cropped_lb, height, width
+
+
+def bbox_cutmix(self, img, labels, h, w):
     xmin = int(min(labels[:, 1] - labels[:, 3] / 2) * w)
     xmax = int(max(labels[:, 1] + labels[:, 3] / 2) * w)
     ymin = int(min(labels[:, 2] - labels[:, 4] / 2) * h)
     ymax = int(max(labels[:, 2] + labels[:, 4] / 2) * h)
 
-    # empty grid for crop-obj
-    # 1 2 3
-    # 4 5 6
-    # 7 8 9
-    Position = [
-        [0, 0, xmin, ymin],  # 1
-        [xmin, 0, xmax, ymin],  # 2
-        [xmax, 0, w, ymin],  # 3
-        [0, ymin, xmin, ymax],  # 4
-        # 5 is a bbox of all objects
-        [xmax, ymin, w, ymax],  # 6
-        [0, ymax, xmin, h],  # 7
-        [xmin, ymax, xmax, h],  # 8
-        [xmax, ymax, w, h],  # 9
-    ]
-
-    # iter empty grid
-    for p in Position:
-        if torch.rand(1) > CONFIG["p"]:  # probability of applying bbox cutmix
-            continue
-        # 1. crop-obj 생성
-        load_idx = torch.randint(len(self.indices), (1,)).item()  # get a image for crop-obj
-        load_img, a, (h_l, w_l) = load_image(self, load_idx)
-        try:
-            lb = self.labels[load_idx].copy()[
-                torch.randint(len(self.labels[load_idx]), (1,)).item()
-            ]
-        except:
-            print("-" * 30)
-            print(">> bbox_cutmix - index error")
-            print(f" - load index : {load_idx}")
-            print(f" - label : {self.labels[load_idx]}")
-            print(f" - file path : {self.img_files[load_idx]}")
-            print("-" * 30)
-            exit()
-
-        xmin_load = int((lb[1] - lb[3] / 2) * w_l)
-        xmax_load = int((lb[1] + lb[3] / 2) * w_l)
-        ymin_load = int((lb[2] - lb[4] / 2) * h_l)
-        ymax_load = int((lb[2] + lb[4] / 2) * h_l)
-
-        # random margin for each 4 sides
-        offset = torch.randint(
-            low=int(self.img_size / CONFIG["margin_min"]),
-            high=int(self.img_size / CONFIG["margin_max"]),
-            size=(4,),
-        ).numpy()
-        offset_xmin = np.clip(min(xmin_load, offset[0]), 0, offset[0])
-        offset_xmax = np.clip(min(w_l - xmax_load, offset[1]), 0, offset[1])
-        offset_ymin = np.clip(min(ymin_load, offset[2]), 0, offset[2])
-        offset_ymax = np.clip(min(h_l - ymax_load, offset[3]), 0, offset[3])
-
-        # cropped image for the selected object
-        cropped_object = load_img[
-            ymin_load - offset_ymin : ymax_load + offset_ymax,
-            xmin_load - offset_xmin : xmax_load + offset_xmax,
-        ].copy()
-
-        # cropped image size
-        height = cropped_object.shape[0]
-        width = cropped_object.shape[1]
-
-        # label after crop
-        # [class, x, y, w, h]
-        cropped_label = [
-            lb[0],
-            offset_xmin,
-            offset_ymin,
-            xmax_load - xmin_load,
-            ymax_load - ymin_load,
+    area = np.array(
+        [
+            [0, 0, xmin, ymin],  # 1
+            [xmin, 0, xmax, ymin],  # 2
+            [xmax, 0, w, ymin],  # 3
+            [0, ymin, xmin, ymax],  # 4
+            # 5 is the smallest box including all existing boxes
+            [xmax, ymin, w, ymax],  # 6
+            [0, ymax, xmin, h],  # 7
+            [xmin, ymax, xmax, h],  # 8
+            [xmax, ymax, w, h],  # 9
         ]
+    )
 
-        # 2. crop-obj albumentations
-        album_aug = A.Compose(  # data augmentation for crop-obj
-            [
-                ###
-                # you can add your own augmentation
-                ###
-                A.HorizontalFlip(p=0.7),
-                A.VerticalFlip(p=0.7),
-                A.ColorJitter(p=0.7),
-                A.ShiftScaleRotate(shift_limit=0.01, scale_limit=0.1, rotate_limit=5, p=0.7),
-                A.RandomRotate90(p=0.7),
-            ],
-            bbox_params=A.BboxParams(
-                format="coco", label_fields=["class_labels"], min_visibility=CONFIG["aug_vis"]
-            ),
-        )
-        try:
-            transformed = album_aug(
-                image=cropped_object, bboxes=[cropped_label[1:]], class_labels=[cropped_label[0]]
-            )
-        except:
-            print("-" * 30)
-            print(">> bbox_cutmix - aug error")
-            print(f" - bbox coord : {cropped_label[1:]}")
-            print(f" - bbox size(HxW) : {lb[4]*h_l} x {lb[3]*w_l}")
-            print(f" - image size(HxW) : {height} x {width}")
-            print(f" - label : {self.labels[load_idx]}")
-            print(f" - file path : {self.img_files[load_idx]}")
-            print("-" * 30)
-            save_image(cropped_object, cropped_label, f"{SAVE_DIR}/error_{load_idx}_aug")
+    input_x = (0.5 * np.random.rand(1) * (area[:, 2] - area[:, 0])).astype(int) + area[:, 0]
+    input_y = (0.5 * np.random.rand(1) * (area[:, 3] - area[:, 1])).astype(int) + area[:, 1]
+
+    for x, y, p in zip(input_x, input_y, area):
+        if torch.rand(1) > CONFIG["p"]:  # probability of bboxcutmix
             continue
 
-        # No object after augmentation
-        if len(transformed["class_labels"]) == 0:
+        # get random object
+        cropped_img, cropped_lb, height, width = get_random_object(self)
+
+        # when there is no label after augmentation, skip area
+        if cropped_lb == None:
             continue
 
-        # augmented crop-obj
-        # bboxes = [(x,y,w,h), (x,y,w,h), ... ]
-        # class_labels = [ c, c, ... ]
-        cropped_object = transformed["image"]
-        cropped_label = [transformed["class_labels"][0]] + list(transformed["bboxes"][0])
-        # crop-obj size
-        height = cropped_object.shape[0]
-        width = cropped_object.shape[1]
-
-        # 3. get insertion position in empty grid
-        x_img = int(0.5 * torch.rand((1,)).item() * (p[2] - p[0])) + p[0]
-        y_img = int(0.5 * torch.rand((1,)).item() * (p[3] - p[1])) + p[1]
-
-        # 4. crop-obj insertion
-        # when insertion not available
-        if p[2] - x_img <= width or p[3] - y_img <= height:
+        # when cropped_img size is bigger then area
+        if width >= p[2] - x or height >= p[3] - y:
             # resize threshold
-            if (
-                p[2] - x_img > self.img_size / CONFIG["resize_thres"]
-                and p[3] - y_img > self.img_size / CONFIG["resize_thres"]
-            ):
-                scale_rate = min((p[2] - x_img) / width, (p[3] - y_img) / height)
+            if p[2] - x > CONFIG["resize_size"] and p[3] - y > CONFIG["resize_size"]:
+                scale_rate = min((p[2] - x) / width, (p[3] - y) / height)
                 album_resize = A.Compose(
                     [A.Resize(int(height * scale_rate), int(width * scale_rate))],
                     bbox_params=A.BboxParams(format="coco", label_fields=["class_labels"]),
                 )
-                try:
-                    transformed = album_resize(
-                        image=cropped_object,
-                        bboxes=[cropped_label[1:]],
-                        class_labels=[cropped_label[0]],
-                    )
-                except:
-                    print("-" * 30)
-                    print(">> bbox_cutmix - resize error")
-                    print(f" - crop coord : {cropped_label[1:]}")
-                    print(f" - crop size(HxW) : {cropped_label[4]} x {cropped_label[3]}")
-                    print(f" - image size(HxW) : {height} x {width}")
-                    print(f" - label : {self.labels[load_idx]}")
-                    print(f" - file path : {self.img_files[load_idx]}")
-                    print("-" * 30)
-                    save_image(
-                        cropped_object, cropped_label, f"{SAVE_DIR}/error_{load_idx}_resize"
-                    )
-                    continue
-
+                transformed = album_resize(
+                    image=cropped_img,
+                    bboxes=[cropped_lb[1:]],
+                    class_labels=[cropped_lb[0]],
+                )
                 # resized crop-obj
-                cropped_object = transformed["image"]
-                cropped_label = [transformed["class_labels"][0]] + list(transformed["bboxes"][0])
-
-                # resizd crop-obj size
-                height = cropped_object.shape[0]
-                width = cropped_object.shape[1]
-            else:  # if over resize threshold, skip
+                cropped_img = transformed["image"]
+                height = cropped_img.shape[0]
+                width = cropped_img.shape[1]
+                cropped_lb = [transformed["class_labels"][0]] + list(transformed["bboxes"][0])
+            else:
                 continue
 
-        # normalize lables for image size
-        cropped_label = [
-            cropped_label[0],
-            (x_img + cropped_label[1] + cropped_label[3] / 2) / w,  # (coords + margin + w/2)
-            (y_img + cropped_label[2] + cropped_label[4] / 2) / h,  # (coords + margin + h/2)
-            cropped_label[3] / w,
-            cropped_label[4] / h,
-        ]
+        # normailze label
+        cropped_lb[1] = (x + cropped_lb[1] + cropped_lb[3] / 2) / w
+        cropped_lb[2] = (y + cropped_lb[2] + cropped_lb[4] / 2) / h
+        cropped_lb[3] = cropped_lb[3] / w
+        cropped_lb[4] = cropped_lb[4] / h
 
-        # add labels
-        labels = np.vstack((labels, np.array([cropped_label])))
-
-        # insert cropped image to empty grid
-        try:
-            img[y_img : y_img + height, x_img : x_img + width] = cropped_object
-        except:
-            print("-" * 30)
-            print(">> bbox_cutmix - insert error")
-            print(f" - crop coord : {cropped_label[1:]}")
-            print(f" - crop size(HxW) : {height} x {width}")
-            print(f" - min point : ({y_img}, {x_img})")
-            print(f" - max point : ({y_img+height}, {x_img+width})")
-            print(f" - image size(HxW) : {img.shape[1]} x {img.shape[0]}")
-            print(f" - label : {self.labels[load_idx]}")
-            print(f" - crop image path : {self.img_files[load_idx]}")
-            print(f" - file path : {self.img_files[index]}")
-            print("-" * 30)
-            save_image(
-                img.copy(),
-                labels,
-                f"{SAVE_DIR}/error_{load_idx}_{index}_insert",
-                p=Position,
-            )
-            continue
-
-        # bbox check
-        if index in range(10, 101, 10):
-            save_image(img.copy(), labels, f"{SAVE_DIR}/monitor_{index}", p=Position)
+        # paste cropped_img, add cropped_lb
+        img[y : y + height, x : x + width] = cropped_img
+        labels = np.vstack((labels, np.array([cropped_lb])))
 
     return img, labels
